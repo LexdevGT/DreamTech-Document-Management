7fefa524f195dd17af5b7a79918ae5159c08d20e
adonahy <adonahyskate@gmail.com>
2022-09-06 08:10:18 -0600

    reporte y categoria
    reporte

--- assets/js/core.js
+++ assets/js/core.js
@@ -2204,7 +2204,7 @@ function load_sidebar(){
                                   '<ul class="nav flex-column sub-menu">'+
                                     '<li class="item"><a class="nav-link" href="#"><i class="mdi mdi-google-assistant menu-icon"><span class="menu-title">Roles</span></i></a></li>'+
                                     '<li class="item"> <a class="nav-link" href="#"><i class="mdi mdi-account-multiple menu-icon"><span class="menu-title">Usuarios</span></i></a></li>'+
-                                    
+                                    '<li class="item"> <a class="nav-link" href="categorias.html"><i class="mdi mdi-account-multiple menu-icon"><span class="menu-title">Categoria</span></i></a></li>'+
                                   '</ul>'+
                                 '</div>'+
                               '</li>'+
